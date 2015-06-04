91391ca16fb9a50221309434c0ba82d66e1de8fe
Kelly Raila <kelly.raila@gmail.com>
2015-06-04 15:46:21 -0400

    Shorten, prepare for submit

--- models/recipe.rb
+++ models/recipe.rb
@@ -12,7 +12,6 @@ class Recipe
     recipe_list = db_connection do |conn|
       conn.exec("SELECT * FROM recipes;")
     end
-
     recipe_instances = []
     recipe_list.each do |recipe_info|
       recipe_instances << Recipe.new(recipe_info["id"], recipe_info["name"], recipe_info["instructions"], recipe_info["description"])
@@ -23,25 +22,19 @@ class Recipe
   def self.find(id)
     recipe_info = db_connection do |conn|
       sql_query ="SELECT recipes.name, recipes.instructions, recipes.description FROM recipes WHERE recipes.id = $1"
-
       conn.exec_params(sql_query, [id])
     end
-
     recipe_ingredients = db_connection do |conn|
       sql_query= "SELECT ingredients.name AS ingredients FROM ingredients WHERE ingredients.recipe_id = $1"
       conn.exec_params(sql_query, [id])
     end
-
     name = recipe_info[0]["name"]
     instructions = recipe_info[0]["instructions"]
     description = recipe_info[0]["description"]
-
     ingredients = []
     recipe_ingredients.each do |hash|
       ingredients << Ingredient.new(hash["ingredients"])
     end
-
     Recipe.new(id, name, instructions, description, ingredients)
   end
-
 end
